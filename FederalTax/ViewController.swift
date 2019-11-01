d8189d9ebc14387319b2906ba958a9abd99d061d
Wesley Sun <wsun@lcs.on.ca>
2019-11-01 11:05:21 -0400

    Initial Commit

---
+++ FederalTax/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FederalTax
+//
+//  Created by Sun, Wesley on 2019-11-01.
+//  Copyright © 2019 Wesley Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
