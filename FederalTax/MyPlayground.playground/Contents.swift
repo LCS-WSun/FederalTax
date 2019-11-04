f803231f8c6bd9274ee71fa467ee54d4595cb995
Wesley Sun <wsun@lcs.on.ca>
2019-11-04 11:53:06 -0500

    Connected the view to the code
    did guard statements
    
    started switch statements- currently here

---
+++ FederalTax/MyPlayground.playground/Contents.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+var name = Wesley
+
+
+
+
+Var amountOf2019FederalTax =
+
+    
+    
+
+
+
+//15% on the first $47,630 of taxable income
+   
+let amountOf2019FederalTax = 
+switch amountOf2019FederalTax {
+    
+case 1:
+    
+case 2:
+    
+case 3:
+    
+case 4:
+
+    
+    
+    
+    
+    
+    
+    
+}
+   
+   
+   
+   
+   
+   //ask the user for their name and gross income (income prior to tax deductions).
+   ● determine their amount of 2019 Federal Tax
+   ● Current federal tax rates are:
+   ○ 15% on the first $47,630 of taxable income
+   ○ 20.5% on the next $47,629 of taxable income (on the portion of taxable income
+   over 47,630 up to and including $95,259), plus
+   ○ 26% on the next $52,408 of taxable income (on the portion of taxable income
+   over $95,259 up to and including $147,667), plus
+   ○ 29% on the next $62,704 of taxable income (on the portion of taxable income
+   over 147,667 up to and including $210,371), plus
+   ○ 33% of taxable income over $210,371
+   ● The application should catch the input of no name and show an appropriate error
+   message.
+   ● The application should prevent the input of an invalid income (negative values, words
+   instead of numbers).
+   ● Otherwise the application should report their Federal Tax amount owing (rounded to 2
+   decimal places) and the effective tax rate ( total tax payable / income ) as a percentage
+   (rounded to 1 decimal place).
+   
--- FederalTax/ViewController.swift
+++ FederalTax/ViewController.swift
@@ -8,13 +8,87 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController { 
+
+    
+    // MARK: Properties
+        
+        // properties is where we keep variables
+        
+        @IBOutlet weak var EnterName: UITextField!
+        
+
+        @IBOutlet weak var AnnualIncome: UITextField!
+        
+        
+        @IBOutlet weak var errormessage: UILabel!
+
+    // MARK: initalizers
+        
+        //
+        
+
+        
+    // MARK: Methods (functions) - behaviors
+        
+        
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        func CalculateTax(_ sender: Any) {
+            
+            guard let EnterNameString = EnterName.text else { return
+                
+                guard let AnnualIncomeString = AnnualIncome.text else {
+                    return
+                }
+            
+                guard let AnnualIncome = Double(AnnualIncomeString) else {
+                    return
+               }
+        
+                switch AnnualIncome {
+                case 1: 0..47630:
+                    0.15 * AnnualIncome
+                    
+                case 2: 47630..95,259
+                0.20.5 * AnnualIncome
+                
+                case 3: 52,408..95259
+                    
+                    
+                    
+                    
+                case 4:
+                    
+                    
+                    
+                default:
+                    <#code#>
+                    
+                    
+                
+                }
+            
+            
+            
+      
+            
+            
+            
+        
     }
+    
 
+
+    
+    
+    
 
 }
 
+}
+}
